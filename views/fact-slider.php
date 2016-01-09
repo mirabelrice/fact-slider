00db317e6febf048db2a365e72ed6dcc0322ebad
Mirabel Rice <rice122@mail.chapman.edu>
2016-01-08 16:36:18 -0800

    started default styles and html

--- views/fact-slider.php
+++ views/fact-slider.php
@@ -7,9 +7,28 @@
 
 	if( !class_exists( FactSliderView ) ) {
 		class FactSliderView {
-			public static function render() {
-				
-			}
+			public static function render() { ?>
+				<div class="fact-slider">
+					<div class="fact-slider-viewport">
+						<ul class="slide-container">
+							<li class="slide">
+								<div class="text-wrap">
+									<div class="slide-text"></div>
+								</div>
+								<!-- LOOP THROUGH ALL SLIDES AND ADD AS LIST ITEMS-->
+							</li>
+						</ul>
+					</div>
+					<div class="slide-controls">
+						<div class="control-nav">
+							<ul>
+								<li class="slide-pager"></li>
+							</ul>
+						</div>
+						<!-- INSERT DIRECTIONAL NAV-->
+					</div>
+				</div>
+			<?php }
 		}
 	}
 ?>
